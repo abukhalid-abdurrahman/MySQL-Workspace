1d348bc41b8a1c84ae67a1924a2f4a2dd88ff24a
faha-berdiev <madysoftware@gmail.com>
2020-05-13 04:51:25 +0300

    REST-API Registration

---
+++ MySQL - Workspace/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ *
+ *  Web Host: ru.000webhost.com
+ *  Host Name: sendysocialclub.000webhostapp.com
+ *  Project Name: SendySocialClub
+ *  Password: m!jg0n@j0n2802
+ *  DataBase Name: id13668343_sendysocialclub
+ *  DataBase Username: id13668343_faridun
+ *  DataBase Password: |Zl^Q8!P{R*ANxyU
+ * 
+ */
+using System;
+using System.Threading;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+namespace MySQL___Workspace
+{
+    class Program
+    {
+        private static string endPoint = "https://sendysocialclub.000webhostapp.com/manager.php";
+
+        private static void InsertRecord(string name, string surname)
+        {
+            Thread requestThread = new Thread(() =>
+            {
+                WebClient webClient = new WebClient();
+
+                NameValueCollection formData = new NameValueCollection();
+                formData["name"] = name;
+                formData["surname"] = surname;
+
+                byte[] responseData = webClient.UploadValues(endPoint, "POST", formData);
+                string responseFromServer = Encoding.UTF8.GetString(responseData);
+
+                Console.WriteLine("Response From Server: " + responseFromServer);
+
+                webClient.Dispose();
+            });
+            requestThread.Start();
+            requestThread.Join();
+        }
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Input Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Input Surname: ");
+                string surname = Console.ReadLine();
+                InsertRecord(name, surname);
+            }
+        }
+    }
+}
---
+++ MySQL - Workspace/Web Site/manager.php
@@ -0,0 +1,21 @@
+﻿<?php
+    $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+    $surname = filter_var(trim($_POST['surname']), FILTER_SANITIZE_STRING);
+    
+    
+    ini_set('mysql.connect_timeout', 300);
+    ini_set('default_socket_timeout', 300);
+    
+    $mysql = new mysqli('localhost', 'id13668343_faridun', '|Zl^Q8!P{R*ANxyU', 'id13668343_sendysocialclub');
+    if (mysqli_connect_errno($mysql))
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    else
+    {
+        echo "OK"; 
+    }
+    
+    $mysql->query("INSERT INTO `User` (`Name`, `Surname`) VALUES('$name', '$surname')");
+    $mysql->close();
+?>
